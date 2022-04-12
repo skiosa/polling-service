447520ca16586ae1c833163581ba513e89d9528a
Jannik Springer <77719392+Jacky-Ickx@users.noreply.github.com>
2022-04-12 12:21:57 +0200

    SKIOS-106 ORM in Polling Service (#4)
    * made code work with shared orm.
    
    * fixed code-smells annotated by SonarQube.
    
    * Changed ORM Import for Fix Version and removed unused Import
    
    Co-authored-by: LukasLJL <lukas@leucali.net>

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 export class rawRSS {
     content!: string
-}
-
-@Entity()
-export class parsedRSS {
-    @PrimaryGeneratedColumn()
-    id!: number;
-
-    @Column()
-    article!: string;
 }
--- tests/unit/parser.spec.ts
+++ tests/unit/parser.spec.ts
@@ -1,11 +1,14 @@
 import { assert } from "chai";
+import { Article } from "skiosa-orm";
 import { parseFeed } from "../../src/parsing";
-import { parsedRSS } from "../../src/types";
 
 describe('Test: parse a raw feed', () => {
     it('should return a parsed feed', async () => {
         let parsedFeed = await parseFeed({content: 'test'});
-        assert.instanceOf(parsedFeed, parsedRSS, 'no parsed feed was returned');
+        assert.instanceOf(parsedFeed, Array, 'no Array was returned');
+        parsedFeed.forEach(article => {
+            assert.instanceOf(article, Article, 'contents of returned Array include non-Article Objects');
+        });
         // console.log(parsedFeed); // uncomment, when you want to look at the output
     });
 });
